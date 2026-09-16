96c9dce2ca0a83cbb9cc5ad0333940ba5a9d3dcd
Lolsifla <148916071+Lolsifla@users.noreply.github.com>
2026-09-16 00:11:13 -0300

    Delete Pre-Entrega-01 directory

--- Pre-Entrega-01/js/main.js
+++
@@ -1,8 +0,0 @@
-let nombre = prompt("Ingrese su nombre:");
-let apellido = prompt("Ingrese su apellido:");
-const añoNacimiento = parseInt(prompt("Ingrese su año de nacimiento:"));
-const añoActual = 2026;
-
-console.log("Nombre completo: " + nombre + " " + apellido);
-
-console.log("Edad: " + (añoActual - añoNacimiento));
